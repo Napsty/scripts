5bce39a7572e5d9bde9eb7fbc13b0b38ce0bf1d9
Günter Bailey <guenter.bailey@bailey-solution.com>
2020-08-07 09:58:21 +0200

    remove docker parts and complexity

--- zoneminder/zoneminder-event-cleanup.sh
+++ zoneminder/zoneminder-event-cleanup.sh
@@ -5,30 +5,25 @@
 # Authors:	Claudio Kuenzler (2018)
 # Doc:		https://www.claudiokuenzler.com/blog/814/how-to-manually-clean-up-delete-zoneminder-events
 # History:
-# 2020-08-06 changed for zoneminder 1.24+ (@Brawn1)
-# 2020-07-24 added Docker Mysql (@Brawn1)
-# 2018-12-18 DB Name variable in first query
 # 2018-12-14 First version
+# 2018-12-18 DB Name variable in first query
+# 2020-07-24 added Docker Mysql (@Brawn1)
+# 2020-08-06 changed for zoneminder 1.24+ (@Brawn1)
+# 2020-08-06 remove complexity and docker parts (@Brawn1)
 ###########################################################################
 # User variables
-olderthan=7 # Defines the minimum age in days of the events to be deleted
-zmcache=/var/lib/docker/volumes/zoneminder_zmcache/_data/events # Defines the path where zm stores events
+olderthan=2 # Defines the minimum age in days of the events to be deleted
+zmcache=/var/cache/zoneminder/events # Defines the path where zm stores events
 mysqlhost=localhost # Defines the MySQL host for the zm database
 mysqldb=zm # Defines the MySQL database name used by zm
 mysqluser=zoneminder # Defines a MySQL user to connect to the database
 mysqlpass=zm-mysql-password # Defines the password for the MySQL user
-docker_mysql="" # if mysql running as docker container, add the mysql container name (example: docker_mysql=zoneminder_mysql_1), or leafe it blank
 
 # Fixed variables
 tmpfile=/tmp/$RANDOM
 
-if [[ ! -z ${docker_mysql} ]]; then
-  # Get archived events from database
-  declare -a archived=( $(docker exec -it ${docker_mysql} bash -lc 'mysql -N -h ${mysqlhost} -u ${mysqluser} --password=${mysqlpass} -e "select Id from ${mysqldb}.Events where Archived = 1;"') )
-else
-  # Get archived events from database
-  declare -a archived=( $(mysql -N -h ${mysqlhost} -u ${mysqluser} --password=${mysqlpass} -e "select Id from ${mysqldb}.Events where Archived = 1;") )
-fi
+# Get archived events from database
+declare -a archived=( $(mysql -N -h ${mysqlhost} -u ${mysqluser} --password=${mysqlpass} -e "select Id from ${mysqldb}.Events where Archived = 1;") )
 
 # Define find exceptions based on archived events
 i=0
@@ -46,20 +41,16 @@ while read line; do
   eventid=$(echo $line | awk '{print $9}' | awk -F'/' '{print $NF}' | sed "s/\.//g")
   realpath=$(echo $line | awk '{print $9"/"$11}'  | sed "s/\.[0-9]*\///g")
   echo "Deleting Event $eventid"
+  
   # delete events from disk and check if absolute path is similar to zmcache path (prevent to delete /)
   if [[ ${realpath} == ${zmcache}* ]]; then
     rm -rf ${realpath}
   fi
+  
   # delete eventid from database
-  if [[ ! -z ${docker_mysql} ]]; then
-    docker exec -it ${docker_mysql} bash -lc 'mysql -h ${mysqlhost} -N -u ${mysqluser} --password=${mysqlpass} -e "DELETE FROM ${mysqldb}.Events where Id = $eventid"'
-    docker exec -it ${docker_mysql} bash -lc 'mysql -h ${mysqlhost} -N -u ${mysqluser} --password=${mysqlpass} -e "DELETE FROM ${mysqldb}.Frames where EventId = $eventid"'
-    docker exec -it ${docker_mysql} bash -lc 'mysql -h ${mysqlhost} -N -u ${mysqluser} --password=${mysqlpass} -e "DELETE FROM ${mysqldb}.Stats where EventId = $eventid"'
-  else
-    mysql -h ${mysqlhost} -N -u ${mysqluser} --password=${mysqlpass} -e "DELETE FROM ${mysqldb}.Events where Id = $eventid"
-    mysql -h ${mysqlhost} -N -u ${mysqluser} --password=${mysqlpass} -e "DELETE FROM ${mysqldb}.Frames where EventId = $eventid"
-    mysql -h ${mysqlhost} -N -u ${mysqluser} --password=${mysqlpass} -e "DELETE FROM ${mysqldb}.Stats where EventId = $eventid"
-  fi
+  mysql -h ${mysqlhost} -N -u ${mysqluser} --password=${mysqlpass} -e "DELETE FROM ${mysqldb}.Events where Id = $eventid"
+  mysql -h ${mysqlhost} -N -u ${mysqluser} --password=${mysqlpass} -e "DELETE FROM ${mysqldb}.Frames where EventId = $eventid"
+  mysql -h ${mysqlhost} -N -u ${mysqluser} --password=${mysqlpass} -e "DELETE FROM ${mysqldb}.Stats where EventId = $eventid"
 done < $tmpfile
 
 exit 0
